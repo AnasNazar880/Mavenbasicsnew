ee20bd2d31ea14298cb30e454fafb906bc9ada94
AnasNazar880 <102611445+AnasNazar880@users.noreply.github.com>
2023-07-26 08:36:13 +0530

    commit1

---
+++ maven_basics/src/main/java/maven_basics/Additon.java
@@ -0,0 +1,12 @@
+package maven_basics;
+
+public class Additon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=58,b=45;
+int sum=a+b;
+System.out.println(sum);
+	}
+
+}
